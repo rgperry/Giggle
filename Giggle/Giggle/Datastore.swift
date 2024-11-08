74205cfb15c3bc3e06dd36ec042c055ab5d41e57
Matthew Drutis <madrutis@gmail.com>
2024-11-08 12:11:10 -0500

    remove hashable protocol from tag

--- Giggle/Giggle/Datastore.swift
+++ Giggle/Giggle/Datastore.swift
@@ -25,7 +25,7 @@ func convertImageToPNG(_ uiImage: UIImage?) throws -> Data {
 
 // This isn't being used now, but if we wanted to do search solely based on tags, we could with this.
 @Model
-class Tag: Hashable {
+class Tag {
     @Attribute(.unique) var name: String
     @Relationship var memes: [Meme]
     
