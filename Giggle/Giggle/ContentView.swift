6301fcf7a4996649d33a54e5e9e32f5966a62c5e
Karan Arora <karanarora@Karans-Air.attlocal.net>
2024-11-10 15:22:11 -0500

    Fix ContentView after rebase

--- Giggle/Giggle/ContentView.swift
+++ Giggle/Giggle/ContentView.swift
@@ -22,34 +22,6 @@ struct ContentView: View {
 
                 ScrollView {
                     LazyVGrid(columns: GridStyle.grid, spacing: GridStyle.folderRowPadding) {
-                        Folder(folderName: "Favorites", pinned: true)
-                        Folder(folderName: "Recently Shared", pinned: true)
-                        Folder(folderName: "All Giggles", pinned: true)
-                        Folder(folderName: "Sports")
-                    }
-                    .padding(.horizontal, GridStyle.columnPadding)
-                    .padding(.top, GridStyle.searchBarPadding)
-                }
-
-                BottomNavBar()
-            }
-            .background(Colors.backgroundColor.ignoresSafeArea())
-        }.tint(.white)
-    }
-}
-
-struct Folder: View {
-    var folderName: String
-    var pinned: Bool = false
-
-    var body: some View {
-        NavigationStack {
-            VStack {
-                PageHeader(text: "Giggle")
-                SearchBar(text: "Search for a Giggle")
-
-                ScrollView {
-                    LazyVGrid(columns: GridStyle.grid, spacing: GridStyle.folderRowPadding) {
                         FolderItem(text: "Favorites", isPinned: true)
                         FolderItem(text: "Recently Shared", isPinned: true)
                         FolderItem(text: "All Giggles", isPinned: true)
@@ -65,26 +37,6 @@ struct Folder: View {
             }
             .background(Colors.backgroundColor.ignoresSafeArea())
         }.tint(.black)
-    }
-}
-
-struct PinIcon: View {
-    var body: some View {
-        Image(systemName: "pin.fill")
-            .foregroundColor(.gray)
-            .font(.system(size: 35))
-            .offset(x: -71, y: -80)
-
-        .background(Color(red: 104/255, green: 86/255, blue: 182/255).ignoresSafeArea())
-    }
-}
-
-struct PinIcon: View {
-    var body: some View {
-        Image(systemName: "pin.fill")
-            .foregroundColor(.gray)
-            .font(.system(size: 35))
-            .offset(x: -71, y: -80)
     }
 }
 
